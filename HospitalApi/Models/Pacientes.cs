6eef3e3ddd7f4bcd4a25cb22658a3e841d56be0f
Helena <helenagp05@gmail.com>
2024-07-15 09:46:05 +0200

    modificant api

--- HospitalApi/Models/Pacientes.cs
+++ HospitalApi/Models/Pacientes.cs
@@ -7,5 +7,5 @@ public class Pacientes
     public string DNI { get; set;} = null!;
     public string NumSS { get; set;} = null!;
     public bool Estat { get; set; }
-    public Camas? camas { get; set; }
+    public Camas? cama { get; set; }
 }
