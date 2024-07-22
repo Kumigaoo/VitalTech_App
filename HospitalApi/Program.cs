b3e30af90c5991cbe4b5b395271714a8cc077bed
Helena <helenagp05@gmail.com>
2024-07-22 12:15:43 +0200

    configurado cors y el program.cs para generar tokens y poder hacer la autentificación

--- HospitalApi/Program.cs
+++ HospitalApi/Program.cs
@@ -1,24 +1,47 @@
 using HospitalAPI;
 using HospitalApi.Data;
 using Microsoft.EntityFrameworkCore;
+using HospitalAPI.Models;
+using HospitalAPI.DTO;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Configuration;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddCors(opt => opt.AddDefaultPolicy(policy =>
-policy.AllowAnyHeader()
-.AllowAnyMethod()
-.AllowAnyOrigin()));
+    policy.AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowAnyOrigin()));
 
 builder.Services.AddControllers();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddAutoMapper(typeof(MapConfig));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => 
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+
 
 var app = builder.Build();
 
@@ -28,8 +51,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseRouting();
+
 app.UseHttpsRedirection();
 
+app.UseCors();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
