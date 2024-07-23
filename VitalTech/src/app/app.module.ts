4b097fab813dfe5739d175fce60522ec504b35e0
Helena <helenagp05@gmail.com>
2024-07-23 09:30:12 +0200

    autentificación en el angular hecho

---
+++ VitalTech/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { JwtModule } from '@auth0/angular-jwt';
+import { AppComponent } from './app.component';
+import { JwtInterceptor } from './jwt.interceptor';
+
+// Función para obtener el token del almacenamiento local
+export function tokenGetter() {
+  return localStorage.getItem('token');
+}
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClient,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        allowedDomains: ["localhost:5001"],
+        disallowedRoutes: ["localhost:5001/api/auth"]
+      }
+    })
+  ],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
