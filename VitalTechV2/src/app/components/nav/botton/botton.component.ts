2fe2a497e391d6f6f882f2c23f16297dad738eb6
Dev953-H <DevWorking@proton.me>
2024-07-26 14:13:34 +0200

    Bottons

---
+++ VitalTechV2/src/app/components/nav/botton/botton.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-botton',
+  standalone: true,
+  imports: [],
+  templateUrl: './botton.component.html',
+  styleUrl: './botton.component.scss'
+})
+export class BottonComponent {
+
+}
