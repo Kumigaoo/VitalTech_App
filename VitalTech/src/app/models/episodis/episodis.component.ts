e1a1b179f9791be045b718e3e593de8ab4811453
Fer <ferlopeznuno@gmail.com>
2024-09-06 10:05:08 +0200

    episodis

--- VitalTech/src/app/models/episodis/episodis.component.ts
+++ VitalTech/src/app/models/episodis/episodis.component.ts
@@ -6,12 +6,13 @@ import { EpisodiMedic } from '../../interface/episodis-medics.interface';
 import { ConsultesPopupComponent } from '../../pop-ups/consultes-popup/consultes-popup.component';
 import { IngressosPopupComponent } from '../../pop-ups/ingressos-popup/ingressos-popup.component';
 import { MatDialog } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-episodis',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive, FormsModule],
   templateUrl: './episodis.component.html',
   styleUrl: './episodis.component.css'
 })
@@ -19,7 +20,11 @@ import { MatDialog } from '@angular/material/dialog';
 export class EpisodisComponent {
 
   episodis: EpisodiMedic[] = [];
+  originalEpisodis: EpisodiMedic[] = [];
   selectedEpisodi: any;
+
+  searchCriteria: string = "id";
+  searchInput: string = "";
 
   constructor(public dialog: MatDialog,private episodiService: EpisodiService) { }
 
@@ -30,9 +35,57 @@ export class EpisodisComponent {
   loadEpisodis(): void {
     this.episodiService.getEpisodis().subscribe(data => {
       this.episodis = data;
+      this.originalEpisodis = data;
     });
   }
 
+  searchEpisodi(): void {
+
+    if (this.searchInput.trim() === '') {
+      this.loadEpisodis();
+      return;
+    }
+
+    this.episodis = this.originalEpisodis
+
+    let busqueda: EpisodiMedic[] = [];
+
+    switch (this.searchCriteria) {
+      case 'id':
+        for (let i = 0; i < this.episodis.length; i++) {
+          if (this.episodis[i].id.toString().includes(this.searchInput)) {
+            busqueda.push(this.episodis[i]);
+          }
+        }
+        break;
+      case 'dolencia':
+        for (let i = 0; i < this.episodis.length; i++) {
+          if (this.episodis[i].dolencia.toLowerCase().includes(this.searchInput.toLowerCase())) {
+            busqueda.push(this.episodis[i]);
+          }
+        }
+        break;
+     
+    }
+
+    this.episodis = busqueda;
+
+  }
+
+  deleteEpisodi(id: number): void {
+    if (confirm('¿Estás seguro de querer eliminar este episodio médico?')) {
+      this.episodiService.deleteEpisodi(String(id)).subscribe({
+        next: () => {
+          alert('Episodio médico eliminado correctamente.');
+          this.loadEpisodis();
+        },
+        error: (error) => {
+          alert('Error, no se puede eliminar este episodio médico: todavía existen consultas o ingresos.');
+        }
+      });
+    }
+  }
+    
   openConsultes(episodi: any): void {
     this.dialog.open(ConsultesPopupComponent, {
       data: { consultes: episodi.consultes },
@@ -52,6 +105,5 @@ export class EpisodisComponent {
       maxHeight: '500px' 
     });
   }
-
-
+  
 }
