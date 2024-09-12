044c4e52780a62956131bdbf416359193ca1ab9f
Helena <helenagp05@gmail.com>
2024-09-12 10:44:38 +0200

    inicio

--- VitalTech/src/app/common/inicio/inicio.component.ts
+++ VitalTech/src/app/common/inicio/inicio.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLinkActive, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [RouterLinkActive, RouterLink],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
