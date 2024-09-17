3ad172a7ed8900ec3bea1a33cbbd3791ea99b000
MarcExt <marc.ferreres.ext@inetum.com>
2024-09-17 10:25:40 +0200

    NoEstandalone

--- VitalTech/src/app/modules/inicio/cama/cama.module.ts
+++ VitalTech/src/app/modules/inicio/cama/cama.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import {CamaComponent} from "../../../pages/inicio/pages/cama/cama.component";
-import { CamaRoutes} from "./cama-routing.module"
+import {CamasComponent} from "./camas.component";
+import { CamaRoutes } from "./cama-routing.module"
 
 @NgModule ({
     declarations:
