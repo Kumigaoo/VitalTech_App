cd0d90dc7c55f4a73cbff853d7a4dcc36ea6d084
Helena <helenagp05@gmail.com>
2024-09-19 10:50:42 +0200

    av

--- VitalTech/src/app/pages/inicio/pages/cama/pages/registro-cama/registro-cama.component.ts
+++ VitalTech/src/app/pages/inicio/pages/cama/pages/registro-cama/registro-cama.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
-
+import { CamasService } from '../../../../../../service/camas.service';
 @Component({
   selector: 'app-registro-cama',
   templateUrl: './registro-cama.component.html',
@@ -11,18 +11,20 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class RegistroCamaComponent {
   llitForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private http: HttpClient,private router: Router, private route: ActivatedRoute ){
+  constructor(private fb: FormBuilder, private http: HttpClient,private router: Router, private route: ActivatedRoute,private llitService: CamasService){
     this.llitForm = this.fb.group({
       codiLlit: ['', [Validators.required, Validators.minLength(4), Validators.pattern(/^\d{3}[AB]$/)]],
       ocupat: ['', Validators.required],
-      foraDeServei: ['', Validators.required],
       habitacioId: ['', Validators.required],
     });
   }
-
   onSubmit(){
+    if(this.llitForm.invalid){
+      this.llitForm.markAllAsTouched();
+      return;
+    }
     const llitData = this.llitForm.value;
-
+    
     this.http.post('http://localhost:5296/api/Llit', llitData).subscribe({
       next: response => console.log('Llit registrada:', response),
       error: error => alert('ERROR, camps no valids'),
