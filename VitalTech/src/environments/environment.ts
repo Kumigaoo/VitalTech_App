6b2fff472ca9c18e0310758e27f1096dfc379a3c
Yagogrr <ygog5002@gmail.com>
2024-10-22 12:15:35 +0200

    button vitaltech -> goldenfold

--- VitalTech/src/environments/environment.ts
+++ VitalTech/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:5000', // URL de tu API en desarrollo
-  apiFrontGoldenfold : 'http://localhost:4200'
+  apiFrontGoldenfold : 'http://localhost:4201'
 };
