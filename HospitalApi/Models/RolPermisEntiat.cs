5e30530cac357391694e8d2a34a25f42d8a38e17
Eric Ayxendri <eric.wow3000@gmail.com>
2024-10-29 10:09:44 +0100

    ADDING ROL_PERMIS_ENTIAT

---
+++ HospitalApi/Models/RolPermisEntiat.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HospitalAPI.Models
+{
+    public class RolPermisEntiat
+    {
+
+        [Key]        
+        public int Id { get; set; }
+
+        [ForeignKey("Rol")]
+        public int RolId { get; set; }
+        public Rol? Rol { get; set; }
+
+        [ForeignKey("Permis")]
+        public int PermisId { get; set; }
+        public Permis? Permis { get; set; }
+
+        [ForeignKey("Enitat")]
+        public int EnitatId { get; set; }
+        public Entitat? Entiat { get; set; }
+
+    }
+}
