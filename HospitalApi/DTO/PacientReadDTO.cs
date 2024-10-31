e7f7ea01af1c6b44571fab7f571b3bdd1a741df1
Yago Garcia <yagog5002@gmail.com>
2024-10-31 10:30:41 +0100

    pacientes DTO + pacientes controller in progress

--- HospitalApi/DTO/PacientReadDTO.cs
+++ HospitalApi/DTO/PacientReadDTO.cs
@@ -9,11 +9,13 @@ namespace HospitalApi.DTO
         public string NumSS { get; set; } = string.Empty;
         public string Nom { get; set; } = string.Empty;
         public string Cognom1 { get; set; } = string.Empty;
-        public string Cognom2 { get; set; } = string.Empty;
+        public string Cognom2 {  get; set; } = string.Empty;
         public string Sexe { get; set; } = string.Empty;
+        public string Telefono { get; set; } = string.Empty;
+        public Nacionalidad Nacionalidad { get; set; }
+        public string Email {get; set; } = string.Empty;
         public DateTime BirthDay { get; set; }
         public ICollection<EpisodiMedicReferenceDTO>? EpisodisMedics { get; set; }
-
-
+        
     }
 }
