9410126b2b4dd7f6e340dbe400b80334d2eb14bd
Eric Ayxendri <eric.wow3000@gmail.com>
2024-11-06 09:09:04 +0100

    DOING USER CONTROLLER

--- HospitalApi/DTO/PersonalReadDTO.cs
+++ HospitalApi/DTO/PersonalReadDTO.cs
@@ -12,6 +12,6 @@ namespace HospitalApi.DTO
 
         public string Nom { get; set; } = string.Empty;
 
-        public ICollection<ConsultaReferenceDTO>? Consultes { get; set; }
+        public ICollection<PruebaDiagnosticaReadDTO>? Consultes { get; set; }
     }
 }
--- HospitalApi/DTO/UsuariReadDTO.cs
+++ HospitalApi/DTO/UsuariReadDTO.cs
@@ -5,7 +5,7 @@ namespace HospitalApi.DTO
 
         public string Username { get; set; } = string.Empty;
         public string Email {get; set;} = string.Empty;
-        public int RolId { get; set; }
+        public string RolId { get; set; } = string.Empty;
 
     }
 }
