568f1efd45880c4a6b2f270d08414e4e51566562
Dev953-H <DevWorking@proton.me>
2024-11-19 13:45:24 +0100

    Validadors Ingres i Habitacio

--- HospitalApi/DTO/HabitacionReferenceDTO.cs
+++ HospitalApi/DTO/HabitacionReferenceDTO.cs
@@ -1,8 +1,12 @@
-﻿namespace HospitalApi.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HospitalApi.DTO
 {
     public class HabitacionReferenceDTO
     {
+        [Required]
         public int CodiHabitacio { get; set; }
+        [Required]
         public int CapacitatLlits { get; set; }
 
     }
