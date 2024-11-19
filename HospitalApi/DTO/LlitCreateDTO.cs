2510d4c5499f177183e4fa8a7e17c45775baa522
Dev953-H <DevWorking@proton.me>
2024-11-19 13:58:07 +0100

    ValidacioParcialLlitCrea

--- HospitalApi/DTO/LlitCreateDTO.cs
+++ HospitalApi/DTO/LlitCreateDTO.cs
@@ -1,10 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HospitalApi.DTO
 {
     public class LlitCreateDTO
     {
-   
+        [RegularExpression(@"^\d{3}[A-Z]$", ErrorMessage = "El format no es vlaid. Tenen que ser 3 digits seguits d'una lletra majuscula.")]
         public string CodiLlit { get; set; } = string.Empty;
         
         [NotMapped]
