1230181543a879ed5c064db1018f6991af7adc52
Yago García <yagog5002@gmail.com>
2024-11-21 09:51:34 +0100

    enfermers component created

---
+++ GoldenFold/src/app/pages/inicio/pages/administrador-sistema/pages/enfermers/enfermers.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-enfermers',
+  templateUrl: './enfermers.component.html',
+  styleUrl: './enfermers.component.css'
+})
+export class EnfermersComponent {
+
+}
