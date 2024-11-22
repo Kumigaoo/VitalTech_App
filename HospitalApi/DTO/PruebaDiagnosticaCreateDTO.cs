ae193a120420f9363e8629da7399d4942e6f8c71
Dev953-H <DevWorking@proton.me>
2024-11-22 09:48:05 +0100

    Bascics validations Full DTOs Excepte Users validations

--- HospitalApi/DTO/PruebaDiagnosticaCreateDTO.cs
+++ HospitalApi/DTO/PruebaDiagnosticaCreateDTO.cs
@@ -1,13 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HospitalApi.DTO
 {
     public class PruebaDiagnosticaCreateDTO
     {
-        
+        [Required]
         public int MetgeId { get; set; }
+        [Required]
         public int EnfermerId { get; set; }
+        [Required]
         public int EpisodiMedicId { get; set; }
+        [Required]
         public string? Dolencia { get; set; }
     }
 }
