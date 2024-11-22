2b57753535b1196200b0e1942371ed8a5bd44d8c
Alejandro <jagsalejandrog2@gmail.com>
2024-11-22 12:39:29 +0100

    Ale 64

---
+++ HospitalApi/DTO/AdministratiuUpdateDTO.cs
@@ -0,0 +1,10 @@
+namespace HospitalAPI.DTO
+{
+    public class AdministratiuUpdateDTO
+    {
+        public string DNI { get; set; } = string.Empty;
+        public string Nom { get; set; } = string.Empty;
+        public int Telefon { get; set; }
+        public string UsuariId { get; set; } = string.Empty;
+    }
+}
