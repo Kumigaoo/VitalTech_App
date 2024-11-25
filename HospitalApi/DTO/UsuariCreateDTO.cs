cd3cd39647675f3ad6e58544177cfeef4d42acd9
Dev953-H <DevWorking@proton.me>
2024-11-25 09:48:33 +0100

    UsuariCreate Required validations

--- HospitalApi/DTO/UsuariCreateDTO.cs
+++ HospitalApi/DTO/UsuariCreateDTO.cs
@@ -1,11 +1,18 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace HospitalApi.DTO
 {
     public class UsuariCreateDTO
     {
 
+        [Required]
         public string Username { get; set; } = string.Empty;
+        [Required]
         public string Password { get; set; } = string.Empty;
-        public string Email {get; set;} = string.Empty;
+        [Required]
+        public string Email { get; set; } = string.Empty;
+        [Required]
         public string RolId { get; set; } = string.Empty;
 
     }
