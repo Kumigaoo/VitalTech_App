47643e3ad70e60faa9a01a970b728fa5f3b03fa7
Helena <helenagp05@gmail.com>
2024-11-27 09:59:47 +0100

    fixed paciente nacionalidad

--- HospitalApi/Migrations/20241127085906_pacientes_fixed.Designer.cs
+++ HospitalApi/Migrations/20241127085906_pacientes_fixed.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HospitalApi.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20241127084623_pacientes_fixed")]
+    [Migration("20241127085906_pacientes_fixed")]
     partial class pacientes_fixed
     {
         /// <inheritdoc />
