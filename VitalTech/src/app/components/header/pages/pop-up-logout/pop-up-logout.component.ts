39b1edfae3f3016b621cd12ed112c7e63569448b
Dev953-H <DevWorking@proton.me>
2024-11-27 13:33:13 +0100

    Imgs Loguin

---
+++ VitalTech/src/app/components/header/pages/pop-up-logout/pop-up-logout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pop-up-logout',
+  standalone: true,
+  imports: [],
+  templateUrl: './pop-up-logout.component.html',
+  styleUrl: './pop-up-logout.component.css'
+})
+export class PopUpLogoutComponent {
+
+}
