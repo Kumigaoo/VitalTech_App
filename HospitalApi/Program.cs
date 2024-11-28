6f391432603c49fea7385ac520f3b60ae77e5b63
Alejandro <jagsalejandrog2@gmail.com>
2024-11-28 12:35:12 +0100

    Ale 72

--- HospitalApi/Program.cs
+++ HospitalApi/Program.cs
@@ -79,7 +79,7 @@ builder.Services.AddDistributedMemoryCache();
 
 builder.Services.AddSession(options =>
 {
-    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.IdleTimeout = TimeSpan.FromMinutes(420);
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
 });
