862ecb20f01012b1ffc10c85f5420684f4fc55b9
Dev953-H <DevWorking@proton.me>
2024-11-28 12:36:00 +0100

    add session variables for parameters to session

--- VitalTech/src/app/components/header/pages/pop-up-logout/pop-up-logout.component.ts
+++ VitalTech/src/app/components/header/pages/pop-up-logout/pop-up-logout.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
@@ -13,16 +14,30 @@ import { Inject } from '@angular/core';
 export class PopUpLogoutComponent {
 
   private readonly oidcSecurityService = inject(OidcSecurityService);
+  private readonly http = inject(HttpClient);
   isAuthenticated = false;
   nom = '';
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) 
-  {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
 
+  ngOnInit() {
+    this.oidcSecurityService.checkAuth().subscribe(({ isAuthenticated, userData }) => {
+      this.isAuthenticated = isAuthenticated;
+      if (isAuthenticated) {
+        this.nom = userData.name;
+        // Guardar el nombre en la sesión
+        this.http.post('/set-session', { value: this.nom }).subscribe();
+      } else {
+        this.nom = '';
+      }
+    });
   }
 
   onLogout(): void {
-    this.oidcSecurityService.logoff().subscribe((result) => console.log(result));
+    this.oidcSecurityService.logoff().subscribe((result) => {
+      console.log(result);
+      // Limpiar la sesión al cerrar sesión
+      this.http.post('/set-session', { value: '' }).subscribe();
+    });
   }
-
 }
