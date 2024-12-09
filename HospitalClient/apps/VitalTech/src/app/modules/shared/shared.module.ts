ca5318091dfc7bba41d3108acb3c2d53db4a428c
Eric Ayxendri <eric.wow3000@gmail.com>
2024-12-09 12:45:28 +0100

    xD

--- HospitalClient/apps/VitalTech/src/app/modules/shared/shared.module.ts
+++ HospitalClient/apps/VitalTech/src/app/modules/shared/shared.module.ts
@@ -1,30 +1,85 @@
+import { SearchBoxComponent } from './../../../../../GoldenFold/src/app/components/search-box/search-box.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NavComponent } from '../../components/nav/nav.component';
-import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
-import { EnumTranslatePipe } from '../../pipes/enum-translate.pipe';
+
+// Angular Material Imports
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatButtonModule } from '@angular/material/button';
+
+// Angular Material Imports
+
+import {
+  MatDialogModule,
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+import { DialogFormularioComponent } from '../../../../../GoldenFold/src/app/components/Formularios/Paciente/dialog-formulario-paciente-create/dialog-formulario.component';
+import { SnackbarComponent } from '../../../../../GoldenFold/src/app/components/snackbar/snackbar.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatOption } from '@angular/material/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCardTitle } from '@angular/material/card';
+import { MatSelect } from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
 
 @NgModule({
-  declarations: [EnumTranslatePipe],
   imports: [
     CommonModule,
-    ReactiveFormsModule,
-    NavComponent,
-    RouterLinkActive,
     FormsModule,
-    RouterLink,
-    RouterModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatOption,
+    MatCardTitle,
+    MatSelect,
+    MatSelectModule,
+    MatOptionModule,
+    DialogFormularioComponent,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSnackBarModule,
+    SearchBoxComponent,
+    SnackbarComponent,
+    MatCheckboxModule,
   ],
   exports: [
     CommonModule,
-    ReactiveFormsModule,
-    NavComponent,
-    RouterLinkActive,
     FormsModule,
-    RouterLink,
-    RouterModule,
-    EnumTranslatePipe,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatOption,
+    MatCardTitle,
+    MatSelect,
+    MatSelectModule,
+    MatOptionModule,
+    DialogFormularioComponent,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSnackBarModule,
+    SearchBoxComponent,
+    SnackbarComponent,
+    MatCheckboxModule,
+  ],
+  providers: [
+    { provide: MAT_DIALOG_DATA, useValue: {} },
+    { provide: MatDialogRef, useValue: {} },
   ],
 })
 export class SharedModule {}
