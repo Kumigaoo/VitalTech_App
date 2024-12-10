ff52521af7420ef77325d7f61c8afa52850b24ae
Yago García <yagog5002@gmail.com>
2024-12-10 13:46:18 +0100

    css adapted

--- HospitalClient/apps/GoldenFold/src/app/modules/inicio/administrador-sistema/administrador-sistema-routing.module.ts
+++ HospitalClient/apps/GoldenFold/src/app/modules/inicio/administrador-sistema/administrador-sistema-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CamasComponent } from '../../../../../../../libs/pages/Cama/camas.component';
 import { HabitacionesComponent } from '../../../pages/inicio/pages/administrador-sistema/pages/habitaciones/habitaciones.component';
-import { PacientesComponent } from '../../../../../../../libs/pages/Paciente/pacientes.component';
+import { PacientesComponent } from '../../../../../../../libs/pages/pacientes/pacientes.component';
 // import { UsuariosComponent } from '../../../pages/inicio/pages/administrador-sistema/pages/usuarios/usuarios.component';
 import { IngresosComponent } from '../../../pages/inicio/pages/administrador-sistema/pages/ingresos/ingresos.component';
 import { AdministradorSistemaComponent } from '../../../pages/inicio/pages/administrador-sistema/administrador-sistema.component';
--- HospitalClient/apps/GoldenFold/src/app/modules/inicio/administrador-sistema/administrador-sistema.module.ts
+++ HospitalClient/apps/GoldenFold/src/app/modules/inicio/administrador-sistema/administrador-sistema.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { AdministradorSistemaRoute } from './administrador-sistema-routing.module';
 import { CamasComponent } from '../../../../../../../libs/pages/Cama/camas.component';
 import { HabitacionesComponent } from '../../../pages/inicio/pages/administrador-sistema/pages/habitaciones/habitaciones.component';
-import { PacientesComponent } from '../../../../../../../libs/pages/Paciente/pacientes.component';
+import { PacientesComponent } from '../../../../../../../libs/pages/pacientes/pacientes.component';
 // import { UsuariosComponent } from '../../../pages/inicio/pages/administrador-sistema/pages/usuarios/usuarios.component';
 import { IngresosComponent } from '../../../pages/inicio/pages/administrador-sistema/pages/ingresos/ingresos.component';
 import { PruebasComponent } from '../../../pages/inicio/pages/administrador-sistema/pages/consultas/pruebas.component';
