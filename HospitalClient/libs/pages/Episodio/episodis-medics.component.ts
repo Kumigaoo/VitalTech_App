d89847f93321374dd77eb523632a1bc426433fa8
Yago García <yagog5002@gmail.com>
2024-12-13 13:46:51 +0100

    episodio medico names changed

--- HospitalClient/libs/pages/Episodio/episodis-medics.component.ts
+++ HospitalClient/libs/pages/Episodio/episodis-medics.component.ts
@@ -73,7 +73,7 @@ export class EpisodisMedicsComponent implements OnInit, AfterViewInit {
     this.isPortVitalTech = currentPort === '4200';
 
     if (currentPort == '4201'){
-      cssPath = ['/assets/styles/styles.css', '/assets/styles/Episodio/episodis-medics.component.css'];
+      cssPath = ['/assets/styles/styles.css', '/assets/styles/Episodio/4001.component.css'];
       this.displayedColumns = [
         'id',
         'dataObertura',
@@ -89,7 +89,7 @@ export class EpisodisMedicsComponent implements OnInit, AfterViewInit {
       ];
       
     } else {
-      cssPath = ['/assets/styles/styles.css', '/assets/styles/Episodio/episodio.component.css'];
+      cssPath = ['/assets/styles/styles.css', '/assets/styles/Episodio/4000.component.css'];
       this.displayedColumns = [
         'id',
         'dataObertura',
