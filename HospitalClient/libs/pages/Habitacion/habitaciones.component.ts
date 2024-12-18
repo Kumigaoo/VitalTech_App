6af2a5dea3236fc35ea4cefc531d56d8686783ab
Helena <helenagp05@gmail.com>
2024-12-18 12:46:44 +0100

    implementado el abstract en habitaciones

--- HospitalClient/libs/pages/Habitacion/habitaciones.component.ts
+++ HospitalClient/libs/pages/Habitacion/habitaciones.component.ts
@@ -3,162 +3,112 @@ import { DialogCrearHabitacionComponent } from '../../forms/Habitacion/Create/di
 import { CamasDialogComponent } from './../../../apps/GoldenFold/src/app/components/popups/camas-popup';
 import { SnackbarComponent } from './../../../apps/GoldenFold/src/app/components/snackbar/snackbar.component';
 import { HabitacionService } from './../../services/habitacion.service';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { Habitacion } from '../../interfaces/habitacion.interface';
 import { MatTableDataSource } from '@angular/material/table'; // Módulo de tabla de Angular Material
 import { MatPaginator, PageEvent } from '@angular/material/paginator'; // Módulo de paginación de Angular Material
 import { MatSort } from '@angular/material/sort'; // Módulo de ordenación de Angular Material
 import { MatDialog } from '@angular/material/dialog';
 import Swal from 'sweetalert2';
+import { AbstractTableComponent } from '../abstract-logic';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-habitaciones',
   templateUrl: './habitaciones.component.html',
   styleUrls: [],
 })
-export class HabitacionesComponent implements OnInit {
-  @ViewChild(SnackbarComponent) snackbar!: SnackbarComponent; // Referencia al snackbar
-  @ViewChild(MatPaginator) paginator!: MatPaginator; // Referencia al paginador de Angular Material
-  @ViewChild(MatSort) sort!: MatSort; // Referencia al orden de Angular Material
-
-  displayedColumns: string[] = [
-    'NumeroHabitacion',
-    'CapCamas',
-    'Planta',
-    'Camas',
-    'Acciones',
-  ]; // Columnas de la tabla
-  dataSource = new MatTableDataSource<Habitacion>(); // DataSource para la tabla
-
-  totalItems = 0;
-  itemsPerPage = 300;
-  pageIndex = 0;
+export class HabitacionesComponent extends AbstractTableComponent<Habitacion> implements OnInit, AfterViewInit {
 
   isPortVitalTech = false;
 
-  addingHabitacion: Habitacion;
-  habitaciones: Habitacion[] = [];
-  notificacion: string | null = null;
-
-  habitacionParaActualizar: Habitacion | null = null;
-
   constructor(
     private habitacionService: HabitacionService,
-    public dialog: MatDialog
+    public override dialog: MatDialog
   ) {
-    this.addingHabitacion = {
-      codiHabitacio: 0,
-      capacitatLlits: 0,
-      plantaId: 0,
-      llits: [''],
-    };
+    super(); // Llamada al constructor del abstracto
+    this.dialog = dialog;
+    this.addingItem = this.crearItemInicial();
   }
 
   ngOnInit(): void {
-    this.obtenerHabitaciones();
+    this.obtenerItems(this.habitacionService.getAll());
+
+    // Lógica para definir los estilos (CSS)
     const currentPort = window.location.port;
-    let cssPath: string[];
+    let cssPath: string[] = [];
 
     this.isPortVitalTech = currentPort === '4200';
-    if (currentPort == '4201'){
+
+    if (currentPort === '4201') {
       cssPath = ['/assets/styles/styles.css', '/assets/styles/habitacion/habitaciones.component.css'];
-      
     } else {
       cssPath = ['/assets/styles/styles.css', '/assets/styles/habitacion/habitacion.component.css'];
     }
+    
+    // Cargar los estilos específicos del componente
+    this.cargarEstilos(cssPath);
 
-    cssPath.forEach(css => {
-      const link = document.createElement('link');
-      link.rel = 'stylesheet';
-      link.type = 'text/css';
-      link.href = css;
-      document.head.appendChild(link);
-    });
+    // Configuración de las columnas para la tabla
+    this.displayedColumns = [
+      'NumeroHabitacion',
+      'CapCamas',
+      'Planta',
+      'Camas',
+      'Acciones',
+    ];
+}
+
+  crearItemInicial(): Habitacion {
+      return {
+        codiHabitacio: 0,
+        capacitatLlits: 0,
+        plantaId: 0,
+        llits: [''],
+      };
   }
 
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator; // Enlazar el paginador a la tabla
-    this.dataSource.sort = this.sort; // Enlazar la ordenación a la tabla
-  }
-
-  obtenerHabitaciones(): void {
-    this.habitacionService.getAll().subscribe({
-      next: (data: Habitacion[]) => {
-        this.habitaciones = data;
-        this.totalItems = data.length;
-        this.actualizarPagina(0, this.itemsPerPage);
-      },
-      error: (error: any) => {
-        console.error('Error al obtener las habitaciones', error);
-        this.snackbar.showNotification(
-          'error',
-          'Error al obtener las habitaciones'
-        ); // Mostrar notificación de error
-      },
-    });
-  }
-
-  onPaginateChange(event: PageEvent) {
-    this.pageIndex = event.pageIndex;
-    this.itemsPerPage = event.pageSize;
-    this.actualizarPagina(this.pageIndex, this.itemsPerPage);
-  }
-
-  actualizarPagina(pageIndex: number, pageSize: number) {
-    const startIndex = pageIndex * pageSize;
-    const endIndex = startIndex + pageSize;
-    this.dataSource.data = this.habitaciones.slice(startIndex, endIndex);
-  }
-
-  toggleFormularioAgregar() {
-    this.addingHabitacion = {
-      codiHabitacio: 0,
-      capacitatLlits: 0,
-      plantaId: 0,
-      llits: [''],
-    };
-
-    this.dialog
-      .open(DialogCrearHabitacionComponent, {
-        data: this.addingHabitacion,
-      })
-      .afterClosed()
-      .subscribe((habitacionCreada) => {
-        if (habitacionCreada) {
-          this.addingHabitacion = habitacionCreada;
-          this.guardarHabitacion();
-        }
-      });
-  }
-
-  guardarHabitacion() {
-    this.habitacionService.post(this.addingHabitacion).subscribe({
-      next: () => {
-        this.obtenerHabitaciones();
-        this.cerrarFormulario();
-        this.snackbar.showNotification(
-          'success',
-          'Habitación guardada exitosamente'
-        ); // Notificación de éxito
-      },
-      error: (error: any) => {
-        this.snackbar.showNotification(
-          'error',
-          'Error al guardar la habitacion'
-        ); // Notificación de error
-      },
-    });
-  }
-
-  verCamas(habitacion: any): void {
+  verRelaciones(habitacion: Habitacion): void {
     this.dialog.open(CamasDialogComponent, {
       width: '1200px',
-      data: habitacion.llits,
+      data: habitacion.llits,  // Pasar las pruebas diagnósticas como data
     });
+  } 
+
+  obtenerDialogoFormularioRegistro(): any {
+    return DialogCrearHabitacionComponent;
+  }
+  
+  obtenerDialogoFormularioModificacion(): any {
+    return DialogActualizarHabitacionComponent; // Aquí se devuelve el diálogo de modificación específico
+  }
+  
+  obtenerItemsService(): Observable<Habitacion[]> {
+    return this.habitacionService.getAll();
+  }
+  
+  guardarService(item: Habitacion): Observable<any> {
+    return this.habitacionService.post(item);
+  }
+  
+  obtenerIdOriginal(item: Habitacion): number {
+    return item.codiHabitacio; 
+  }
+  
+  actualizarService(id: number, item: Habitacion): Observable<any> {
+    return this.habitacionService.put(id, item);
+  }
+  
+  eliminarService(id: number): Observable<any> {
+    return this.habitacionService.delete(id);
+  }
+  
+  necesitaConfirmacion(): boolean {
+    return this.isPortVitalTech;
   }
 
-  borrarHabitacion(id: number): void {
-    Swal.fire({
+  mostrarConfirmacion(): Promise<any> {
+    return Swal.fire({
       title: 'Eliminar habitación',
       text: '¿Quieres borrar esta habitación?',
       icon: 'warning',
@@ -167,96 +117,19 @@ export class HabitacionesComponent implements OnInit {
       cancelButtonColor: '#d33',
       confirmButtonText: 'Sí',
       cancelButtonText: 'Cancelar',
-    }).then((result) => {
-      if (result.isConfirmed) {
-        this.habitacionService.delete(id).subscribe({
-          next: () => {
-            this.obtenerHabitaciones();
-            this.snackbar.showNotification(
-              'success',
-              'Habitación eliminada correctamente'
-            );
-          },
-          error: () => {
-            console.error('Error al eliminar la habitación');
-            this.snackbar.showNotification(
-              'error',
-              'Error al eliminar la habitación'
-            );
-          },
-        });
-      }
-    });
-  }
-
-  toggleActualizarHabitacion(habitacion: Habitacion): void {
-    if (this.isPortVitalTech){
-      window.location.href = `https://localhost:4200/inicio/habitacion/modif-habitacion/${habitacion.codiHabitacio}`;
-    } else {
-    this.habitacionParaActualizar = { ...habitacion }; // Guardar habitación para actualización
-    this.dialog
-      .open(DialogActualizarHabitacionComponent, {
-        data: this.habitacionParaActualizar,
-      })
-      .afterClosed()
-      .subscribe((habitacionActualizada) => {
-        if (habitacionActualizada) {
-          this.habitacionParaActualizar = habitacionActualizada;
-          this.actualizarhabitacion(habitacion.codiHabitacio);
-        }
       });
     }
-  }
 
-  navigateToRegistroHabitacion(): void {
-    window.location.href = 'https://localhost:4200/inicio/habitacion/registro-habitacion';
-  }
-
-  actualizarhabitacion(id: number): void {
-    if (this.habitacionParaActualizar) {
-      this.habitacionService.put(id, this.habitacionParaActualizar).subscribe({
-        next: () => {
-          this.obtenerHabitaciones();
-          this.cerrarFormulario();
-          this.snackbar.showNotification(
-            'success',
-            'Habitación actualizada correctamente'
-          ); // Notificación de éxito
-        },
-        error: (error: any) => {
-          this.snackbar.showNotification(
-            'error',
-            'Error al actualizar la habitación'
-          ); // Notificación de error
-        },
-      });
-    }
-  }
-
-  cerrarFormulario(): void {
-    this.habitacionParaActualizar = null;
-  }
-
-  aplicarFiltro(event: { type: string; term: string }): void {
-    const { type, term } = event;
-    const searchterm = term.trim().toLowerCase();
-
-    this.dataSource.filterPredicate = (data: Habitacion, filter: string) => {
-      switch (type) {
-        case 'habitacion':
-          return data.codiHabitacio.toString().toLowerCase() === filter;
-        case 'capacitat':
-          return data.capacitatLlits.toString().toLowerCase() === filter;
-        case 'planta':
-          return data.plantaId.toString().toLowerCase() === filter;
-        default:
-          return false;
-      }
-    };
-    this.dataSource.filter = searchterm;
-
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage(); // Reiniciar la paginación cuando se aplica el filtro
+  definirFiltro(data: Habitacion, type: string, filter: string): boolean {
+    switch (type) {
+      case 'habitacion':
+        return data.codiHabitacio.toString().toLowerCase() === filter;
+      case 'capacitat':
+        return data.capacitatLlits.toString().toLowerCase() === filter;
+      case 'planta':
+        return data.plantaId.toString().toLowerCase() === filter;
+      default:
+        return false;
     }
   }
 }
