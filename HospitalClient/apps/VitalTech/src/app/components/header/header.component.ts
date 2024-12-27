cf3627e79cf4161afa9ee80bebe88e0c911a6fd8
Fer <ferlopeznuno@gmail.com>
2024-12-27 10:28:40 +0100

    hi

--- HospitalClient/apps/VitalTech/src/app/components/header/header.component.ts
+++ HospitalClient/apps/VitalTech/src/app/components/header/header.component.ts
@@ -4,8 +4,7 @@ import { RouterLinkActive, RouterLink, Router } from '@angular/router';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
 import { PopUpLogoutComponent } from './pages/pop-up-logout/pop-up-logout.component';
 import { MatDialog } from '@angular/material/dialog';
-import { HttpClient } from '@angular/common/http';
-
+ 
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -15,41 +14,59 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HeaderComponent {
   private readonly oidcSecurityService = inject(OidcSecurityService);
-  private readonly https = inject(HttpClient);
   isAuthenticated = false;
-  nom = '';
-
+  nom: string = '';
+ 
   constructor(private router: Router, public dialog: MatDialog) {}
-
+ 
   ngOnInit() {
     this.oidcSecurityService.checkAuth().subscribe(({ isAuthenticated, userData }) => {
       this.isAuthenticated = isAuthenticated;
-
+ 
       if (isAuthenticated) {
         this.nom = userData?.name || '';
-
+ 
         const data = new Date();
-
+ 
         data.setTime(data.getTime() + 365 * 24 * 60 * 60 * 1000);
-
+ 
         const expires = `expires=${data.toUTCString()}`;
-
+ 
         document.cookie = "nomUser=" + this.nom + `; ${expires}; path=/login`;
-
+ 
       } else {
-
+ 
         this.nom = '';
-
+ 
       }
     });
   }
-
+ 
   onLogin(): void {
     this.oidcSecurityService.authorize();
   }
-
+ 
+  getCookie(name: string): string {
+ 
+    const cookies = document.cookie.split(';');
+ 
+    for (const cookie of cookies) {
+ 
+      const [key, value] = cookie.split('=');
+ 
+      if (key == name) {
+ 
+        return value;
+ 
+      }
+    }
+ 
+    return "Dont exist";
+ 
+  }
+ 
   openDialog(): void {
-
+ 
     this.dialog.open(PopUpLogoutComponent, {
       data: {
         nom: this.getCookie("nomUser")
--- HospitalClient/apps/VitalTech/src/app/components/header/pages/pop-up-logout/pop-up-logout.component.ts
+++ HospitalClient/apps/VitalTech/src/app/components/header/pages/pop-up-logout/pop-up-logout.component.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Inject } from '@angular/core';
-
+ 
 @Component({
   selector: 'app-pop-up-logout',
   standalone: true,
@@ -16,17 +16,17 @@ export class PopUpLogoutComponent {
   private readonly https = inject(HttpClient);
   isAuthenticated = false;
   nom: string = '';
-
+ 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
-
+ 
     this.nom = data.nom;
-
+ 
   }
-
+ 
   onLogout(): void {
     this.oidcSecurityService.logoff().subscribe((result) => {
       console.log(result);
-
+ 
       // Limpiar la sesión al cerrar sesión
       document.cookie = `nomUser=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/login;`;
     });
