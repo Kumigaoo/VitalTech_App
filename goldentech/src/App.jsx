9f15008f5508e7e6d9739d8bbb5ce6981cd173d2
Fer <ferlopeznuno@gmail.com>
2025-01-13 13:49:49 +0100

    footer&header

---
+++ goldentech/src/App.jsx
@@ -0,0 +1,12 @@
+import Footer from './pages/shared/footer/Footer'
+import Header from './pages/shared/header/header';
+
+export default function App() {
+    return (
+        <>
+            <Header/>
+            <Footer/>
+        </>
+          
+    );
+}
---
+++ goldentech/src/pages/shared/footer/Footer.jsx
@@ -0,0 +1,25 @@
+
+
+export default function Footer() {
+  console.log("Footer is rendering");
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-logo">
+          <img src="/assets/goldentech.png" alt="GoldenTech Logo" />
+        </div>
+        <div className="footer-contact">
+          <h4>Contáctanos</h4>
+          <p>
+            Email: consultas@goldentech.com
+            
+          </p>
+          <p>Phone: +1 234 567 890</p>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>© 2025 GoldenTech. Todos los derechos reservados</p>
+      </div>
+    </footer>
+  );
+}
