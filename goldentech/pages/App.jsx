1f3303452102466a69a66efb650f69397a86e1ed
Helena <helenagp05@gmail.com>
2025-01-14 13:55:08 +0100

    routes

---
+++ goldentech/pages/App.jsx
@@ -0,0 +1,13 @@
+import MainContent from "./MainContent";
+import Footer from '../src/pages/shared/footer/Footer'
+import Header from '../src/pages/shared/header/header';
+
+export default function App() {
+    return (
+        <>
+            <Header/>
+            <MainContent />
+            <Footer/>
+        </>        
+    );
+}
