893bddf2e7eeaf2ac1d42d97bd5bb189fc4ffc01
Eric Ayxendri <eric.wow3000@gmail.com>
2025-01-15 11:22:35 +0100

    afegit login comentat

---
+++ goldentech/src/components/login/login.jsx
@@ -0,0 +1,44 @@
+/*
+import React from 'react';
+import Keycloak from 'keycloak-js';
+import { ReactKeycloakProvider, useKeycloak } from '@react-keycloak/web';
+
+const keycloak = new Keycloak({
+    realm: 'Dream Team',
+    url: 'https://login.oscarrovira.com/realms/Dream%20Team',
+    clientId: 'hospital-api',
+});
+
+const Login = () => {
+    
+    const { keycloak, initialized } = useKeycloak();
+
+    if (!initialized) {
+        return <div>Loading...</div>;
+    }
+
+    if (!keycloak.authenticated) {
+        return (
+            <div>
+                <p>You are not authenticated</p>
+                <button onClick={() => keycloak.login()}>Login</button>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <p>Welcome, {keycloak.tokenParsed?.name}</p>
+            <button onClick={() => keycloak.logout()}>Logout</button>
+        </div>
+    );
+};
+
+const LoginPage = () => (
+    <ReactKeycloakProvider authClient={keycloak}>
+        <Login />
+    </ReactKeycloakProvider>
+);
+
+export default LoginPage;
+/*
