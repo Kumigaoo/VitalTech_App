25ada49c35d64f0cdff58071a0aabc7da1266b79
Alejandro <jagsalejandrog2@gmail.com>
2025-01-15 12:28:13 +0100

    Ale 89

--- HospitalClient/apps/GoldenFold/src/app/modules/inicio/administrador-sistema/administrador-sistema.module.ts
+++ HospitalClient/apps/GoldenFold/src/app/modules/inicio/administrador-sistema/administrador-sistema.module.ts
@@ -7,18 +7,16 @@ import { CamasComponent } from '../../../../../../../libs/pages/Cama/camas.compo
 import { HabitacionesComponent } from '../../../../../../../libs/pages/Habitacion/habitaciones.component';
 import { PacientesComponent } from '../../../../../../../libs/pages/pacientes/pacientes.component';
 import { UsuarioComponent } from '../../../../../../../libs/pages/Usuario/usuario.component';
-import { IngresosComponent } from '../../../pages/inicio/pages/administrador-sistema/pages/ingresos/ingresos.component';
+import {IngresosComponent} from '../../../../../../../libs/pages/Ingreso/ingresos.component'
 import { PruebasComponent } from '../../../../../../../libs/pages/Pruebas-diagnosticas/pruebas.component';
 import { AdministradorSistemaDashboardComponent } from '../../../pages/inicio/pages/administrador-sistema/administrador-sistema-dashboard/administrador-sistema-dashboard.component';
 import { SharedModule } from '../../shared/shared.module';
-import { AdministradorSistemaComponent } from '../../../pages/inicio/pages/administrador-sistema/administrador-sistema.component';
 import { MetgesComponent } from '../../../../../../../libs/pages/Medico/metges.component';
 import { EnfermersComponent } from '../../../../../../../libs/pages/Enfermero/enfermers.component';
 import { EpisodiComponent } from '../../../../../../../libs/pages/Episodio/episodio-medico.component';
 
 @NgModule({
   declarations: [
-    AdministradorSistemaComponent,
     MetgesComponent,
     CamasComponent,
     AdministradoresSistemaComponent,
--- HospitalClient/apps/VitalTech/src/app/modules/inicio/ingreso/ingreso.module.ts
+++ HospitalClient/apps/VitalTech/src/app/modules/inicio/ingreso/ingreso.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { IngresoRoutes } from './ingreso-routing.module';
-import { IngresoComponent } from '../../../pages/inicio/pages/ingreso/ingreso.component';
+import { IngresosComponent } from '../../../../../../../libs/pages/Ingreso/ingresos.component';
 import { SharedModule } from '../../shared/shared.module';
 import { RegistroIngresoComponent } from '../../../pages/inicio/pages/ingreso/pages/registro-ingres/registro-ingreso.component';
 import { ModifIngresoComponent } from '../../../pages/inicio/pages/ingreso/pages/modif-ingres/modif-ingreso.component';
+import { AdministradorSistemaDashboardComponent } from '../../../../../../GoldenFold/src/app/pages/inicio/pages/administrador-sistema/administrador-sistema-dashboard/administrador-sistema-dashboard.component';
+import { CommonModule } from '@angular/common';
+import { MatPaginator } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [
-    IngresoComponent,
+    IngresosComponent,
     RegistroIngresoComponent,
     ModifIngresoComponent,
   ],
-  imports: [IngresoRoutes, SharedModule],
+  imports: [IngresoRoutes, CommonModule, SharedModule, AdministradorSistemaDashboardComponent, MatPaginator],
 })
 export class IngresoModule {}
