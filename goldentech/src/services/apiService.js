af4cf28b54756828f3cbb0a1dd544f8701a2a6e0
Fer <ferlopeznuno@gmail.com>
2025-01-23 12:17:05 +0100

    mas documentacion

--- goldentech/src/services/apiService.js
+++ goldentech/src/services/apiService.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+//se define la url de la api:
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://localhost:7200/api';
 
 const api = axios.create({
@@ -9,23 +10,25 @@ const api = axios.create({
   },
 });
 
-// Interceptor para manejar los errores de respuesta
+//movida para manejar errores:
 api.interceptors.response.use(
-  response => response, // Si la respuesta es correcta, la pasa directamente
+  response => response, //si la respuesta es correcta, la pasa directamente
   error => {
-    console.error('Axios error:', error.response || error.message); // Muestra el error en consola
-    return Promise.reject(error); // Rechaza la promesa con el error
+    console.error('Axios error:', error.response || error.message); //en caso de que se produzca 1 error, lo muestra
+    return Promise.reject(error); 
   }
 );
 
-// Funciones para interactuar con la API
+//métodos para obtener pacientes y pacientes x id:
 export const getPacients = async () => {
   try {
+    //recuperamos de la api todos los pacientes y los guardamos en una const:
     const response = await api.get('/Pacient');
+    //returneamos response.data. el .data es algo agregado por Axios: contiene la respuesta del servidor, es decir, toda la info del paciente:
     return response.data;
   } catch (error) {
     console.error('Error obteniendo pacientes:', error);
-    throw error; // Re-lanza el error para que lo maneje el componente
+    throw error; 
   }
 };
 
@@ -39,4 +42,5 @@ export const getPacientById = async (id) => {
   }
 };
 
+//esto permite que podamos importar la api en otros archivos sin necesidad de usar llaves {} : 
 export default api;
